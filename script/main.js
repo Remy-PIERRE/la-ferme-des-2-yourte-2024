5ec9c3cb783bb2e1fb1cc5a3988b6c731bfb093e
Remy Pierre <remy.pierre.dev@outlook.fr>
2024-01-02 19:54:08 +0100

    patch images pour changer

--- script/main.js
+++ script/main.js
@@ -38,21 +38,21 @@ const carrouselData = [
 	{
 		title: "Titre de test 1",
 		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-		image: "../public/images/mouton_portrait.jpg",
+		image: "./public/images/mouton_portrait.jpg",
 		ctaText: "BOUTIQUE",
 		ctaLink: "./boutique.html",
 	},
 	{
 		title: "Titre de test 2",
 		description: "Description test 2",
-		image: "../public/images/sheep_2.jpg",
+		image: "./public/images/sheep_2.jpg",
 		ctaText: "&Agrave; PROPOS",
 		ctaLink: "./boutique.html",
 	},
 	{
 		title: "Titre de test 3",
 		description: "Description test 3",
-		image: "../public/images/sheep_3.jpg",
+		image: "./public/images/sheep_3.jpg",
 		ctaText: "&Agrave; PROPOS",
 		ctaLink: "./boutique.html",
 	},
@@ -63,7 +63,7 @@ const shopData = [
 		id: 1,
 		title: "titre 1",
 		description: "description 1",
-		image: "../public/images/mouton_portrait.jpg",
+		image: "./public/images/mouton_portrait.jpg",
 		fav: true,
 	},
 	{
@@ -71,21 +71,21 @@ const shopData = [
 		title: "titre 2",
 		description:
 			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis commodi eaque ipsum rem, doloremque ut.",
-		image: "../public/images/sheep_2.jpg",
+		image: "./public/images/sheep_2.jpg",
 		fav: true,
 	},
 	{
 		id: 3,
 		title: "titre 3",
 		description: "description 3",
-		image: "../public/images/mouton_portrait.jpg",
+		image: "./public/images/mouton_portrait.jpg",
 		fav: true,
 	},
 	{
 		id: 4,
 		title: "titre 4",
 		description: "description 4",
-		image: "../public/images/sheep_3.jpg",
+		image: "./public/images/sheep_3.jpg",
 		fav: true,
 	},
 ];
